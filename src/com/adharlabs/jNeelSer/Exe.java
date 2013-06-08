263b9c259582383e2a1c065c231531e04a9473b7
boseji <boseji@users.noreply.github.com>
2013-06-08 17:52:58 +0530

    One level working Tx Rx tested

--- src/com/adharlabs/jNeelSer/Exe.java
+++ src/com/adharlabs/jNeelSer/Exe.java
@@ -1,13 +1,35 @@
 /**
  * jNeelSer - Java Full loaded Serial Port Terminal
- * 
+ *
  */
 package com.adharlabs.jNeelSer;
 
 import com.adharlabs.UI.VisualTerminal;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.AbstractAction;
+import javax.swing.ButtonGroup;
+import javax.swing.InputMap;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.KeyStroke;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
 
 /**
  *
@@ -16,37 +38,225 @@ import java.util.logging.Logger;
 public class Exe {
 
     @SuppressWarnings("NonConstantLogger")
-    public static Logger LOG;
+    private static Logger LOG;
+    private NeelSerOptions opt;
+    private JRadioButtonMenuItem[] arck_port;
+    private VisualTerminal vs;
+    private JMenuItem jmi_OpenClose;
+    private StringBuilder sRxStream;
+
     /**
      * Static block to Configure the logger by default
      */
-    static{
+    static {
         try {
             com.adharlabs.log.LogCustomHandler.setup(Exe.class.getName());
         } catch (IOException ex) {
             System.err.println("Error could not intialize the logger:\n"
-                    +ex.toString());
-            System.exit(1);            
+                    + ex.toString());
+            System.exit(1);
         }
     }
+
+    private int getSelectPort() {
+        int i;
+        for (i = 0; i < arck_port.length; i++) {
+            if (arck_port[i].isSelected()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void Action_setPortName(ActionEvent e) {
+        String s = ((javax.swing.AbstractButton) e.getSource()).getText();
+        if (opt.indexOfPortName(s) != -1 && (!opt.isPortOpen)) {
+            //Set the port Index as per the string
+            opt.iSelectedPortIndex = opt.indexOfPortName(s);
+        } else {
+            //Restore the Older Selection
+            for (int i = 0; i < arck_port.length; i++) {
+                if (i == opt.iSelectedPortIndex) {
+                    arck_port[i].setSelected(true);
+                }
+            }
+        }
+    }
+
+    private void Action_serialevent(SerialPortEvent se) {
+        if (se.isRXCHAR() || se.isRXFLAG()) {
+            String s;
+            try {
+                s = opt.xPort.readString();
+                if (s != null) {
+                    if (s.indexOf("\b") != -1) {
+                        try {
+                            if (vs.styledoc.getLength() > 0) {
+                                vs.styledoc.remove(vs.styledoc.getLength() - 1, 1);
+                                //Fix Set the default style at the last char location
+                                SimpleAttributeSet aset = new SimpleAttributeSet();
+                                StyleConstants.setForeground(aset, Color.black);
+                                StyleConstants.setBackground(aset, Color.WHITE);
+                                vs.jtextpane.setCharacterAttributes(aset, false);
+                            }
+                        } catch (BadLocationException ex) {
+                            Exe.LOG.log(Level.SEVERE, "Error: ", ex);
+                        } catch (Exception ex) {
+                            Exe.LOG.log(Level.SEVERE, "Error: ", ex);
+                        }
+                    } else {
+                        try {
+                            vs.styledoc.insertString(vs.styledoc.getLength(), s, null);
+                        } catch (BadLocationException ex) {
+                            
+                        }
+                    }
+                    if (opt.param_bEnableLoopBack) {
+                        opt.xPort.writeString(s);
+                    }
+                    vs.gotoend();
+                }
+            } catch (SerialPortException ex) {
+                Exe.LOG.log(Level.SEVERE, "Problem in Receiving data", ex);
+            }
+        }
+    }
+
+    private void Action_openclose(ActionEvent e) {
+        if (getSelectPort() != -1) {
+            if (!opt.isPortOpen) {
+                opt.iSelectedPortIndex = getSelectPort();
+                try {
+                    opt.openPort();
+                    jmi_OpenClose.setText("Close");
+                    vs.println("Opened Port: " + opt.sPortName, Color.MAGENTA);
+                    opt.xPort.addEventListener(new SerialPortEventListener() {
+                        @Override
+                        public void serialEvent(SerialPortEvent serialPortEvent) {
+                            Action_serialevent(serialPortEvent);
+                        }
+                    },
+                            SerialPort.MASK_RXCHAR | SerialPort.MASK_RXFLAG);
+                } catch (Exception ex) {
+                    Exe.LOG.log(Level.INFO, "Port Open Failed\n{0}", ex.toString());
+                }
+            } else {
+                try {
+                    opt.closePort();
+                    jmi_OpenClose.setText("Open");
+                    vs.println("Closed Port: " + opt.sPortName, Color.MAGENTA);
+                } catch (Exception ex) {
+                    Exe.LOG.log(Level.INFO, "Port Close Failed \n{0}", ex);
+                }
+            }
+        }
+    }
+
+    public Exe() {
+        // Initialize the Local logger
+        Exe.LOG = com.adharlabs.log.LogCustomHandler.LOG;
+        Exe.LOG.log(Level.INFO, "Begin Exe");
+        Exe.LOG.setLevel(Level.ALL);//Enable Debug Logs
+
+        // Create the Visual Terminal
+        vs = new VisualTerminal(400, 600, "jNeelSer",
+                Exe.LOG);
+        opt = new NeelSerOptions(Exe.LOG);
+        sRxStream = new StringBuilder();
+
+        //Create Menu Bar
+        JMenuBar jmb = new JMenuBar();
+        //Add Menu Bar to the Visual Terminal
+        vs.setJMenuBar(jmb);
+
+        //Create Port List Setting
+        JMenu jPort = new JMenu("Port Sel");
+        jmb.add(jPort);
+        ButtonGroup bg1 = new ButtonGroup();
+        try {
+            arck_port = opt.refreshPortList_menu();
+            jPort.removeAll();
+            for (int i = 0; i < arck_port.length; i++) {
+                jPort.add(arck_port[i]);
+                bg1.add(arck_port[i]);
+                if (i == opt.iSelectedPortIndex) {
+                    arck_port[i].setSelected(true);
+                }
+                arck_port[i].addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Action_setPortName(e);
+                    }
+                });
+            }
+        } catch (Exception e) {
+            vs.println(e.toString(), Color.red);
+        }
+
+        jmi_OpenClose = new JMenuItem("Open");
+        jmi_OpenClose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Action_openclose(e);
+            }
+        });
+        jmb.add(jmi_OpenClose);
+        KeyStroke xks_Send = KeyStroke.getKeyStroke(KeyEvent.VK_SPACE,
+                java.awt.event.InputEvent.CTRL_DOWN_MASK);
+        vs.jtextpane.getInputMap().put(xks_Send, "actionMapKey");
+
+        vs.jtextpane.getActionMap().put("actionMapKey",
+                new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (opt.isPortOpen) {
+                    vs.println();//Go to a New Line To indicate Sending
+                    //Get the Final Buffer
+                    String s = sRxStream.toString().trim() + "\r\n";
+                    // Message fir the Text being sent
+                    vs.println("Sending Text: " + s, Color.PINK);
+
+                    try {
+                        //Attempt to Really send the string
+                        opt.xPort.writeString(s);
+                        //Clear the Buffer
+                        sRxStream.delete(0, sRxStream.capacity());
+                    } catch (SerialPortException ex) {
+                        Exe.LOG.log(Level.SEVERE, "Failed to send String: "
+                                + s + "\n{0}", ex);
+                    }
+                }
+            }
+        });
+
+        vs.jtextpane.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                char ch = (char) e.getKeyChar();
+                // Only add incase any special keys are not pressed
+                if (!e.isAltDown() && !e.isAltGraphDown()
+                        && !e.isControlDown() && !e.isMetaDown()
+                        && opt.isPortOpen) {
+                    if ((ch >= 32) && (ch < 127)) {
+                        sRxStream.append(ch);
+                    } else if (ch == '\n') {
+                        sRxStream.append('\n');
+                    }
+                }
+            }
+        });
+        //opt.param_bEnableLoopBack = true;
+        opt.iBaudrate = SerialPort.BAUDRATE_115200;
+        vs.jtextpane.setAutoscrolls(true);
+        // Start Execution
+        vs.setVisible(true);
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Initialize the Local logger
-        Exe.LOG = com.adharlabs.log.LogCustomHandler.LOG;
-        Exe.LOG.log(Level.INFO,"Begin Exe");
-        Exe.LOG.setLevel(Level.ALL);//Enable Debug Logs
-        // Create the Visual Terminal
-        VisualTerminal vs = new VisualTerminal(200, 200, "jNeelSer", 
-                Exe.LOG);
-        
-        // Add some dummy lines
-        vs.println("Hello This is the first lines\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+
-                "\n\n\n\n\n\n\n\n\n\n");
-        
-        // Start Execution
-        vs.setVisible(true);
+        Exe exe;
+        exe = new Exe();
     }
 }
--- src/com/adharlabs/jNeelSer/NeelSerOptions.java
+++ src/com/adharlabs/jNeelSer/NeelSerOptions.java
@@ -1,6 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/* jNeelSer Option and Data Storage class
+ * This class encloses the data related to the serial port and
+ * data interation options. It also provides a wrapper for the
+ * Serial port related functions
+ * @copy © Abhijit Bose , A.D.H.A.R Labs Research Bharat(India), 2013.
+ *
+ * NeelSerOptions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeelSerOptions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @email info@adharlabs.in
+ * @website http://adharlabs.in
+ *
  */
 package com.adharlabs.jNeelSer;
 
@@ -8,23 +27,38 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
+import javax.swing.JRadioButtonMenuItem;
+import jssc.SerialPort;
+import jssc.SerialPortException;
 import jssc.SerialPortList;
 
 /**
+ * jNeelSer Option and Data Storage class
  *
- * @author bose
+ * @version 1.0_20130625
+ * @author Abhijit Bose , A.D.H.A.R Labs Research Bharat(India)
  */
 public class NeelSerOptions {
 
     @SuppressWarnings("NonConstantLogger")
     private static Logger LOG;
+    public SerialPort xPort;
     public boolean isPortOpen = false;
     public String[] arsPortList;
+    public int iSelectedPortIndex;
     public String sPortName;
     public int iBaudrate;
     public int iParity;
     public int iStopBits;
+    public int iDataBits;
     public int iFlowControl;
+    public boolean bCTS;
+    public boolean bRTS;
+    public boolean bDSR;
+    public boolean bDTR;
+    public boolean bRI;
+    public boolean bRSLD;
+    public boolean param_bEnableLoopBack;
 
     public NeelSerOptions(Logger log) {
         // Assign the Primary Things
@@ -33,28 +67,158 @@ public class NeelSerOptions {
         // Initialize the Parameters
         this.arsPortList = SerialPortList.getPortNames();
         this.sPortName = this.arsPortList[0];
-
+        this.iSelectedPortIndex = 0;
+        this.isPortOpen = false;
+        this.iBaudrate = SerialPort.BAUDRATE_9600;
+        this.iParity = SerialPort.PARITY_NONE;
+        this.iStopBits = SerialPort.STOPBITS_1;
+        this.iDataBits = SerialPort.DATABITS_8;
+        this.iFlowControl = SerialPort.FLOWCONTROL_NONE;
+        this.bCTS = false;
+        this.bRTS = false;
+        this.bDSR = false;
+        this.bDTR = false;
+        this.bRI = false;
+        this.bRSLD = false;
+        this.param_bEnableLoopBack = false;
+        
         //Finally done
         NeelSerOptions.LOG.fine("Done Initialization");
     }
 
-    public void refreshPortList(JMenu jm) throws Exception {
+    public int indexOfPortName(String s) {
+        boolean b = false;
+        int i;
+        for (i = 0; i < this.arsPortList.length; i++) {
+            if (this.arsPortList[i].equalsIgnoreCase(s)) {
+                b = true;
+                break;
+            }
+        }
+        if (b) {
+            return i;
+        }
+        return -1;
+    }
+
+    public boolean openPort(/*int index*/) throws Exception {
+        // In Future add Semaphore Lock (Relese Sem in Exceptions)
+        //Check if port is already opened
+        if (this.isPortOpen) {
+            NeelSerOptions.LOG.log(Level.FINE, "Port Already Opened, Need to"
+                    + " close the port first Index:{0}", /*index*/
+                    this.iSelectedPortIndex);
+            return false;
+        }
+
+        // Get the serial port name using the index
+        this.sPortName = this.arsPortList[this.iSelectedPortIndex];
+        /*
+         try {
+         this.sPortName = this.arsPortList[index];
+         } catch (ArrayIndexOutOfBoundsException aiob) {
+         String s = "Index " + index + " provided for "
+         + "Serial port selection is outside the current"
+         + " SerialPort Names list\n Exception: "
+         + aiob.toString();
+         NeelSerOptions.LOG.log(Level.WARNING, s);
+         throw new Exception(s);
+         }
+         */
+        //Create the Serial Port
+        this.xPort = new SerialPort(this.sPortName);
+
+        //Try to open the port
+        try {
+            this.isPortOpen = this.xPort.openPort();
+        } catch (SerialPortException ser1) {
+            String s = "Unable to open the port for the "
+                    + "following Reason\n Exception:" + ser1.toString();
+            NeelSerOptions.LOG.log(Level.WARNING, s);
+            this.isPortOpen = false;
+            throw new Exception(s);
+        }
+
+        //Set the Parameters
+        try {
+            this.xPort.setParams(this.iBaudrate, this.iDataBits, this.iStopBits,
+                    this.iParity, this.bRTS, this.bDTR);
+        } catch (SerialPortException ser2) {
+            String s = "Unable to Set Parameters for following "
+                    + "Reason\n Exception:" + ser2.toString();
+            NeelSerOptions.LOG.log(Level.WARNING, s);
+            throw new Exception(s);
+        }
+
+        //Set Flow Control
+        try {
+            this.xPort.setFlowControlMode(this.iFlowControl);
+        } catch (SerialPortException ser3) {
+            String s = "Unable to set Flow control for the  "
+                    + "following Reason\n Exception:" + ser3.toString();
+            NeelSerOptions.LOG.log(Level.WARNING, s);
+            throw new Exception(s);
+        }
+
+        //Clear the port
+        try {
+            this.xPort.purgePort(SerialPort.PURGE_RXCLEAR | SerialPort.PURGE_TXCLEAR);
+        } catch (SerialPortException ser4) {
+            String s = "Unable to Purge the Serial port for the "
+                    + "following Reason\n Exception:" + ser4.toString();
+            NeelSerOptions.LOG.log(Level.WARNING, s);
+            this.isPortOpen = false;
+            throw new Exception(s);
+        }
+        // In Future add Semaphore Release
+        return true;
+    }
+
+    public boolean closePort() throws Exception {
+
+        // Check if the port was open
+        if (!this.isPortOpen) {
+            NeelSerOptions.LOG.log(Level.FINE, "Port Already Closed, Need to"
+                    + " open the port first");
+            return false;
+        }
+
+        // Close the port
+        try {
+            this.isPortOpen = false;
+            this.xPort.closePort();
+        } catch (SerialPortException ser) {
+            String s = "Unable to Close the Serial port for the "
+                    + "following Reason\n Exception:" + ser.toString();
+            NeelSerOptions.LOG.log(Level.WARNING, s);
+            throw new Exception(s);
+        }
+
+        return true;
+    }
+
+    public JRadioButtonMenuItem[] refreshPortList_menu() throws Exception {
         // Get the Port List
         this.arsPortList = SerialPortList.getPortNames();
         // Clear the menue
-        jm.removeAll();
-        // Variable to check if the port thats currently open 
+        JRadioButtonMenuItem[] arck = new JRadioButtonMenuItem[this.arsPortList.length];
+        // Variable to check if the port thats currently open
         //   is present in the list
         boolean foundopenport = false;
+        int i = 0;
         // Cycle through the Port Names
         for (String s : arsPortList) {
             // Set the Check box in case the port currently opened is found
             if (this.isPortOpen && this.sPortName.equals(s)) {
-                jm.add(new JCheckBoxMenuItem(s, null, true));
+                arck[i] = new JRadioButtonMenuItem(s, null, true);
                 foundopenport = true;
+                NeelSerOptions.LOG.log(Level.FINER,
+                        "Found Currently opened port: "
+                        + "{0} in the port list", this.sPortName);
             } else { //In normal case none would be selected
-                jm.add(new JCheckBoxMenuItem(s, null, false));
+                arck[i] = (new JRadioButtonMenuItem(s, null, false));
             }
+            ++i;
         }
 
         if (this.isPortOpen) // If the Port currently open is not found in the list Raise Exeption
@@ -68,8 +232,9 @@ public class NeelSerOptions {
             } else // In case there is just a refresh and no ports are open
             //  Make default selection
             {
-                ((JCheckBoxMenuItem) jm.getItem(0)).setSelected(true);
+                arck[0].setSelected(true);
             }
         }
+        return arck;
     }
 }
